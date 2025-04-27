15410436aa164088ab17e7cf6f86cfe6eb6f7bc7
Cameron Ortega <137089034+Stiver32@users.noreply.github.com>
2025-04-27 16:23:36 +0000

    submission

--- DACArecipient.java
+++ DACArecipient.java
@@ -1,69 +1,53 @@
-/**
- * Represents one person receiving deferred action for childhood arrivals (DACA).
- * 
- * <TODO add @author info here for all group-mates!>
- *
- * @version 1.2
- **/
+public class DACArecipient {
+    private String surname;
+    private String givenName;
+    private String uscisNumber;
+    private String countryOfOrigin;
+    private int birthday, validFromDate, expirationDate;
+    private char sex;
 
-//TODO: Complete UML class diagram
-/* UML CLASS DIAGRAM:
------------------------------------------
-					DACArecipient
------------------------------------------
-- surname : String
-- givenName : String
-- uscisNumber : String
-- countryOfOrigin : String
-- birthday : int
-- validFromDate : int
-- expirationDate : int
-- sex : char
------------------------------------------
-+ getSurname() : String
-+ getGivenName() : String
-+ getUscisNumber() : String
-+ getCountyOfOrigin() : String
-+ getBirthDay() : int
-+ getValidFromDate() : int
-+ getExpirationDate() : int
-+ getSex() : char
-+ setSurname(surname : String) : void
-+ setGivenName(givenName : String) : void
-+ setUscisNumber(uscisNumber : String) : void
-+ setCountryOfOrigin(countryOfOrigin : String) : void
-+ setBirthday(birthday : int) : void
-+ setValidFromDate(validFromDate : int) : void
-+ setExpirationDate(expirationDate : int) : void
-+ setSex(sex : char) : void
-+ setAll(surname:String, givenName:String, uscisNumber:String, countryOfOrigin:String, birthday:int, validFromDate:int, expirationDate:int,sex:char) : void
-+ toString() : String
-+ equals(other : DACArecipient) : boolean
-+ printCard() : void
-+ jdnToDate(J : int) : String
------------------------------------------
-*/
+    // Default constructor
+    public DACArecipient() {
+        surname         = "Unknown";
+        givenName       = "Unknown";
+        uscisNumber     = "Unknown";
+        countryOfOrigin = "Unknown";
+        birthday        = 2415021;  // JDN for 1/1/1900
+        validFromDate   = 2415021;
+        expirationDate  = 2415021;
+        sex             = 'X';
+    }
 
-public class DACArecipient
-{
-	/***** INSTANCE VARIABLES *****/
-  private String surname;
-  private String givenName;
-  private String uscisNumber;
-  private String countryOfOrigin;
-  private int birthday, validFromDate, expirationDate;
-  private char sex;
-	
+    // Full constructor
+    public DACArecipient(
+        String surname,
+        String givenName,
+        String uscisNumber,
+        String countryOfOrigin,
+        int birthday,
+        int validFromDate,
+        int expirationDate,
+        char sex
+    ) {
+        this.surname         = surname;
+        this.givenName       = givenName;
+        this.uscisNumber     = uscisNumber;
+        this.countryOfOrigin = countryOfOrigin;
+        this.birthday        = birthday;
+        this.validFromDate   = validFromDate;
+        this.expirationDate  = expirationDate;
+        this.sex             = sex;
+    }
 
-	//TODO: don't forget to copy all your methods from the previous labs!
-	
-  /**** CONSTRUCTORS ****/
-  //TODO: Write the no-argument constructor and full constructor. Remember to include documentation for each method.
-
-
-	/***** OTHER REQUIRED METHODS *****/
-  public String toString()
-  {
-    return "Surname: " + surname + ", Given Name: " + givenName + ", USCIS Number: " + uscisNumber + ", Country of Origin: " + countryOfOrigin + ", Birthday: " + birthday + ", Valid From Date: " + validFromDate + ", Expiration Date: " + expirationDate + ", Sex: " + sex;
-  }
+    @Override
+    public String toString() {
+        return "Surname: "       + surname
+             + ", Given Name: "  + givenName
+             + ", USCIS Number: "+ uscisNumber
+             + ", Country: "     + countryOfOrigin
+             + ", Birthday: "    + birthday
+             + ", Valid From: "  + validFromDate
+             + ", Expiration: "  + expirationDate
+             + ", Sex: "         + sex;
+    }
 }
--- Main.java
+++ Main.java
@@ -4,10 +4,28 @@
 *	PROGRAM DESCRIPTION:
 *	This program is to test methods of the DACArecipient class.
 *********************************************/
-class Main 
-{
-  public static void main(String[] args) 
-	{
+// Main.java
+public class Main {
+    public static void main(String[] args) {
+        // Test default constructor
+        DACArecipient defaultRecipient = new DACArecipient();
+        System.out.println("Default Constructor:");
+        System.out.println(defaultRecipient);
 
-  }
+        System.out.println();
+
+        // Test full constructor
+        DACArecipient fullRecipient = new DACArecipient(
+            "Smith",
+            "John",
+            "A123456789",
+            "Mexico",
+            2420000,
+            2425000,
+            2430000,
+            'M'
+        );
+        System.out.println("Full Constructor:");
+        System.out.println(fullRecipient);
+    }
 }
